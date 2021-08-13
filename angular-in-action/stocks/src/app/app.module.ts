49bb1935692899edbf5432eacd04785570b9bfe3
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-08-13 19:09:24 +0300

    feat: (angular-in-action) (stocks) add a summary component

--- angular-in-action/stocks/src/app/app.module.ts
+++ angular-in-action/stocks/src/app/app.module.ts
@@ -5,10 +5,12 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {StocksService} from './service/stocks/stocks.service';
+import { SummaryComponent } from './component/summary/summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
