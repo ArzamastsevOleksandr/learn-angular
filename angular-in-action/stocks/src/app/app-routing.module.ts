8de9ca5f0c9040ba6620f1c20d334b2a1d0064a7
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-08-14 00:05:03 +0300

    feat: (angular-in-action) (stocks) add routing

--- angular-in-action/stocks/src/app/app-routing.module.ts
+++ angular-in-action/stocks/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from './component/dashboard/dashboard.component';
+import {ManageComponent} from './component/manage/manage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: 'manage',
+    component: ManageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
