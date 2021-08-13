929b71a3753a19920c0ea1127726ded7a8ec47fc
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-08-13 23:21:38 +0300

    feat: (angular-in-action) (stocks) add a manage component

--- angular-in-action/stocks/src/app/app.module.ts
+++ angular-in-action/stocks/src/app/app.module.ts
@@ -5,19 +5,23 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {StocksService} from './service/stocks/stocks.service';
-import { SummaryComponent } from './component/summary/summary.component';
-import { DashboardComponent } from './component/dashboard/dashboard.component';
+import {SummaryComponent} from './component/summary/summary.component';
+import {DashboardComponent} from './component/dashboard/dashboard.component';
+import {ManageComponent} from './component/manage/manage.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     SummaryComponent,
-    DashboardComponent
+    DashboardComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [StocksService],
   bootstrap: [AppComponent]
